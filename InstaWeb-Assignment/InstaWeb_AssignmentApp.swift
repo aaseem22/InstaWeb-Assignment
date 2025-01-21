582d9c7beced1e2f9534ed207fddc2009c475916
aaseem22 <aaseem.mhaskar.am@gmail.com>
2025-01-21 20:02:57 +0530

    Initial Commit

---
+++ InstaWeb-Assignment/InstaWeb_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaWeb_AssignmentApp.swift
+//  InstaWeb-Assignment
+//
+//  Created by Aaseem Mhaskar on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct InstaWeb_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstaWeb-AssignmentTests/InstaWeb_AssignmentTests.swift
@@ -0,0 +1,16 @@
+//
+//  InstaWeb_AssignmentTests.swift
+//  InstaWeb-AssignmentTests
+//
+//  Created by Aaseem Mhaskar on 21/01/25.
+//
+
+import Testing
+
+struct InstaWeb_AssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
